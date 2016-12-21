d593d7f6d1de992776041b551b55e5569e6c912b
Jashan S <jashan1998s@gmail.com>
2016-12-21 07:02:11 +0530

    A1 Perfected to 100%

--- src/Week1/A1/Percolation.java
+++ src/Week1/A1/Percolation.java
@@ -1,35 +1,35 @@
 package Week1.A1;
 
+import Week1.WeightedQuickUnion;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
  * Created by Jashan Shewakramani
  *
- * Description: Completely rewritten model with cleaner implementation and path compression
+ * Description: Percolation without Path Compression to get 100 on assignment
  */
 public class Percolation {
     private int virtualTopIndex;
     private int dimension;
     private int virtualBottomIndex;
-    private int[] forest; // we'll be adding our own path compression
-    private int[] sizes;
+    private WeightedQuickUnionUF forest;
+    private WeightedQuickUnionUF forest2;
     private boolean[] openSites;
 
     public Percolation(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("N cannot be less than zero");
         dimension = n;
         virtualTopIndex = n * n;
         virtualBottomIndex = n * n + 1;
 
-        forest = new int[n * n + 2];
+        forest = new WeightedQuickUnionUF(n * n + 2);
+        forest2 = new WeightedQuickUnionUF(n * n + 2);
         openSites = new boolean[n * n + 2];
-        sizes = new int[n * n + 2];
 
         openSites[virtualTopIndex] = true;
         openSites[virtualBottomIndex] = true;
 
-        for (int i = 0; i < forest.length; i++) {
-            forest[i] = i;
-            sizes[i] = 1;
-        }
     }
 
     private int siteIndex(int row, int col) {
@@ -47,72 +47,49 @@ public class Percolation {
         if (isOpen(row, col))
             return;
 
+
         int index = siteIndex(row, col);
         openSites[index] = true;
 
-        if (row == 1)
-            union(index, virtualTopIndex);
+        if (row == 1) {
+            forest.union(index, virtualTopIndex);
+            forest2.union(index, virtualTopIndex);
+        }
+
+        if (row == dimension)
+            forest.union(index, virtualBottomIndex);
 
 
         // make possible adjacent unions
-        makePossibleUnion(index, row+1, col);
-        makePossibleUnion(index, row-1, col);
-        makePossibleUnion(index, row, col+1);
-        makePossibleUnion(index, row, col-1);
+        makePossibleUnion(index, row+1, col, forest);
+        makePossibleUnion(index, row-1, col, forest);
+        makePossibleUnion(index, row, col+1, forest);
+        makePossibleUnion(index, row, col-1, forest);
+
+        // make possible adjacent unions
+        makePossibleUnion(index, row+1, col, forest2);
+        makePossibleUnion(index, row-1, col, forest2);
+        makePossibleUnion(index, row, col+1, forest2);
+        makePossibleUnion(index, row, col-1, forest2);
 
     }
 
     public boolean isFull(int row, int col) {
-        return connected(siteIndex(row, col), virtualTopIndex);
+        return forest2.connected(siteIndex(row, col), virtualTopIndex);
     }
 
     public boolean percolates() {
-        for (int i = 1; i <= dimension; i++) {
-            if (connected(siteIndex(dimension, i), virtualTopIndex))
-                return true;
-        }
-
-        return false;
+        return forest.connected(virtualTopIndex, virtualBottomIndex);
     }
 
-    private void makePossibleUnion(int index, int tRow, int tCol) {
+    private void makePossibleUnion(int index, int tRow, int tCol, WeightedQuickUnionUF forest) {
         if (tRow <= 0 || tCol <= 0 || tCol > dimension || tRow > dimension)
             return;
 
         if (isOpen(tRow, tCol))
-            union(index, siteIndex(tRow, tCol));
+            forest.union(index, siteIndex(tRow, tCol));
     }
 
-    private int getRoot(int p) {
-        if (forest[p] == p) {
-            return p;
-        } else {
-            forest[p] = getRoot(forest[p]);
-            return forest[p];
-        }
-    }
-
-    private boolean connected(int p, int q) {
-        return getRoot(p) == getRoot(q);
-    }
-
-    private void union(int p, int q) {
-        int pParent = getRoot(p);
-        int qParent = getRoot(q);
-
-        // don't make any changes if they're already connected
-        if (pParent == qParent)
-            return;
-
-        // make the smaller tree a child of the larger tree
-        if (sizes[pParent] < sizes[qParent]) {
-            forest[pParent] = qParent;
-            sizes[qParent] += sizes[pParent];
-        } else {
-            forest[qParent] = pParent;
-            sizes[pParent] += sizes[qParent];
-        }
-    }
 
     public static void main(String[] args) {
         Percolation p = new Percolation(3);
