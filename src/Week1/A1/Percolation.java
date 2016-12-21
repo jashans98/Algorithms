38ac0216abb96d7fa2dbbaedf8ac19f6562facb0
Jashan S <jashan1998s@gmail.com>
2016-12-21 06:21:53 +0530

    Major bug fixes and cleanup

--- src/Week1/A1/Percolation.java
+++ src/Week1/A1/Percolation.java
@@ -1,8 +1,127 @@
 package Week1.A1;
 
+
 /**
  * Created by Jashan Shewakramani
+ *
+ * Description: Completely rewritten model with cleaner implementation and path compression
  */
 public class Percolation {
+    private int virtualTopIndex;
+    private int dimension;
+    private int virtualBottomIndex;
+    private int[] forest; // we'll be adding our own path compression
+    private int[] sizes;
+    private boolean[] openSites;
 
+    public Percolation(int n) {
+        dimension = n;
+        virtualTopIndex = n * n;
+        virtualBottomIndex = n * n + 1;
+
+        forest = new int[n * n + 2];
+        openSites = new boolean[n * n + 2];
+        sizes = new int[n * n + 2];
+
+        openSites[virtualTopIndex] = true;
+        openSites[virtualBottomIndex] = true;
+
+        for (int i = 0; i < forest.length; i++) {
+            forest[i] = i;
+            sizes[i] = 1;
+        }
+    }
+
+    private int siteIndex(int row, int col) {
+        if (row <= 0 || col <= 0 || row > dimension || col > dimension)
+            throw new IndexOutOfBoundsException("Invalid coordinates!");
+
+        return (row - 1) * dimension + (col - 1);
+    }
+
+    public boolean isOpen(int row, int col) {
+        return openSites[siteIndex(row, col)];
+    }
+
+    public void open(int row, int col) {
+        if (isOpen(row, col))
+            return;
+
+        int index = siteIndex(row, col);
+        openSites[index] = true;
+
+        if (row == 1)
+            union(index, virtualTopIndex);
+
+
+        // make possible adjacent unions
+        makePossibleUnion(index, row+1, col);
+        makePossibleUnion(index, row-1, col);
+        makePossibleUnion(index, row, col+1);
+        makePossibleUnion(index, row, col-1);
+
+    }
+
+    public boolean isFull(int row, int col) {
+        return connected(siteIndex(row, col), virtualTopIndex);
+    }
+
+    public boolean percolates() {
+        for (int i = 1; i <= dimension; i++) {
+            if (connected(siteIndex(dimension, i), virtualTopIndex))
+                return true;
+        }
+
+        return false;
+    }
+
+    private void makePossibleUnion(int index, int tRow, int tCol) {
+        if (tRow <= 0 || tCol <= 0 || tCol > dimension || tRow > dimension)
+            return;
+
+        if (isOpen(tRow, tCol))
+            union(index, siteIndex(tRow, tCol));
+    }
+
+    private int getRoot(int p) {
+        if (forest[p] == p) {
+            return p;
+        } else {
+            forest[p] = getRoot(forest[p]);
+            return forest[p];
+        }
+    }
+
+    private boolean connected(int p, int q) {
+        return getRoot(p) == getRoot(q);
+    }
+
+    private void union(int p, int q) {
+        int pParent = getRoot(p);
+        int qParent = getRoot(q);
+
+        // don't make any changes if they're already connected
+        if (pParent == qParent)
+            return;
+
+        // make the smaller tree a child of the larger tree
+        if (sizes[pParent] < sizes[qParent]) {
+            forest[pParent] = qParent;
+            sizes[qParent] += sizes[pParent];
+        } else {
+            forest[qParent] = pParent;
+            sizes[pParent] += sizes[qParent];
+        }
+    }
+
+    public static void main(String[] args) {
+        Percolation p = new Percolation(3);
+        p.open(1, 1);
+        p.open(2, 2);
+        p.open(2, 1);
+        p.open(3, 1);
+        p.open(3, 3);
+        System.out.println(p.percolates());
+        System.out.println(p.isFull(3, 3));
+    }
 }
--- src/Week1/A1/PercolationStats.java
+++ src/Week1/A1/PercolationStats.java
@@ -1,7 +1,87 @@
 package Week1.A1;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 /**
  * Created by Jashan Shewakramani
+ * Description: Class for running Monte Carlo Simulations on the Percolation Model
  */
 public class PercolationStats {
+
+    private double[] dataset;
+    private int dimension;
+    private int numTrials;
+
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException("arguments must be greater than zero");
+        dimension = n;
+        numTrials = trials;
+        dataset = new double[numTrials];
+        for (int i = 0; i < numTrials; i++) {
+            dataset[i] = runTrial();
+        }
+    }
+
+    // check if you can select stuff in O(1)
+    private double runTrial() {
+
+        Percolation percolation = new Percolation(dimension);
+        int numIters = 0;
+
+        while (!percolation.percolates()) {
+            int encodedSite = StdRandom.uniform(0, dimension * dimension);
+            int row = getRow(encodedSite);
+            int col = getCol(encodedSite);
+
+            if (!percolation.isOpen(row, col)) {
+                percolation.open(row, col);
+                numIters++;
+            }
+        }
+
+        return ((double) numIters) / ((double) (dimension * dimension));
+    }
+
+    private int getRow(int encodedSite) {
+        return (encodedSite / dimension) + 1;
+    }
+
+    private int getCol(int encodedSite) {
+        return (encodedSite % dimension) + 1;
+    }
+
+    public double mean() {
+        return StdStats.mean(dataset);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(dataset);
+    }
+
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(numTrials);
+    }
+
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(numTrials);
+    }
+
+    public static void main(String[] args) {
+        int n;
+        int t;
+        if (args.length == 2) {
+            n = Integer.parseInt(args[0]);
+            t = Integer.parseInt(args[1]);
+        } else {
+            n = 50;
+            t = 500;
+        }
+
+        PercolationStats p = new PercolationStats(n, t);
+        System.out.println(p.mean());
+        System.out.println(p.stddev());
+        System.out.println(p.confidenceLo() + ", " + p.confidenceHi());
+    }
 }
